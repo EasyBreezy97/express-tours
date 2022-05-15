5a09ca9d5617e63c1a46334296640fd9999f3caa
Guram <gurikakhasia@gmail.com>
2022-05-15 21:12:58 +0400

    Add data validation in tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+// const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
     {
@@ -7,6 +8,18 @@ const tourSchema = new mongoose.Schema(
             type: String,
             required: [true, "A tour must have a name"], //validator
             unique: true,
+            maxlength: [
+                40,
+                "A tour name must have less or equal than 40 characters",
+            ],
+            minlength: [
+                10,
+                "A tour name must have more or equal than 10 characters",
+            ],
+            // validate: [
+            //     validator.isAlpha,
+            //     "Tour name must only contain characters",
+            // ],
         },
         slug: String,
         duration: {
@@ -20,10 +33,16 @@ const tourSchema = new mongoose.Schema(
         difficulty: {
             type: String,
             required: [true, "A tour must have a difficulty"],
+            enum: {
+                values: ["easy", "medium", "difficult"],
+                message: "Difficulty is either: easy, medium, difficult",
+            },
         },
         ratingsAvarage: {
             type: Number,
             default: 4.5,
+            min: [1, "Rating must be above 1.0"],
+            max: [5, "Rating must be below 5.0"],
         },
         ratingsQuantity: {
             type: Number,
@@ -33,7 +52,16 @@ const tourSchema = new mongoose.Schema(
             type: Number,
             required: [true, "A tour must have a price"],
         },
-        priceDiscount: Number,
+        priceDiscount: {
+            type: Number,
+            validate: {
+                validator: function (val) {
+                    return val < this.price;
+                },
+                message:
+                    "Discount price ({VALUE}) should be below regular price",
+            },
+        },
         summary: {
             type: String,
             trim: true, //trims strings
