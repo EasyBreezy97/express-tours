79cc89e5b532e266d4c241bd738ade0eaf80cc15
Guram <gurikakhasia@gmail.com>
2022-06-12 19:39:06 +0400

    Add user model

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "User must have a name"],
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, "User must have an email"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email"],
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 8,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, "Please confirm a password"],
+        validate: {
+            //works ONLY on save and create
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: "Passwords are not the same",
+        },
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    // Only run this is password was modified
+    if (!this.isModified("password")) return next();
+    //hash password const of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    //delete password confirm field
+    this.passwordConfirm = undefined;
+
+    next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
